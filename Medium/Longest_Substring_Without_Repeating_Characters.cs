57afe5401e0e7fc211db423b47b79bdfe866bc04
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-01-02 05:33:26 +0300

    ADD Longest_Substring_Without_Repeating_Characters problem #2

---
+++ Medium/Longest_Substring_Without_Repeating_Characters.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        string OriginalText = "pwwkew";
+
+        Console.WriteLine(LengthOfLongestSubstring(OriginalText));
+
+        Console.ReadKey();
+    }
+
+    public static int LengthOfLongestSubstring(string s)
+    {
+        if (s == null || s.Length == 0)
+            return 0;
+
+        int Start = 0; // Start pointer
+        int End = 0;   // End pointer
+        int MaxLength = 0;
+
+        HashSet<char> CharSet = new HashSet<char>();
+
+        while (End < s.Length)
+        {
+            if (!CharSet.Contains(s[End]))
+            {
+                // Expand the window
+                CharSet.Add(s[End]);
+                End++;
+                MaxLength = Math.Max(MaxLength, End - Start);
+            }
+            else
+            {
+                // Shrink the window
+                CharSet.Remove(s[Start]);
+                Start++;
+            }
+        }
+
+        return MaxLength;
+    }
+
+}
+
+
