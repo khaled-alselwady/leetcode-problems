63583e89b1b75959fd1af4801045443df256b236
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-01-09 05:21:27 +0300

    ADD String_To_Integer_(atoi) problem #6

---
+++ Medium/String_To_Integer(atoi).cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        string s = "-2147483649";
+
+        Console.WriteLine(MyAtoi(s));
+
+        Console.ReadKey();
+    }
+
+    public static int MyAtoi(string s)
+    {
+        if (string.IsNullOrWhiteSpace(s))
+            return 0;
+
+        StringBuilder sb = new StringBuilder(s.Trim());
+
+        bool IsPositive = true;
+        byte Index = 0;
+
+        if (Index < sb.Length && (sb[Index] == '+' || sb[Index] == '-'))
+        {
+            IsPositive = (sb[Index] == '+');
+            Index++;
+        }
+
+        int Number = 0;
+
+        for (; Index < sb.Length; Index++)
+        {
+            if (!char.IsDigit(sb[Index]))
+                break;
+
+            // The current digit is => (sb[Index] - '0')
+
+            if (IsPositive)
+            {
+                if (Number > int.MaxValue / 10 ||
+                    (Number == int.MaxValue / 10 && (sb[Index] - '0') > 7))
+                    return int.MaxValue;
+            }
+            else
+            {
+                if ((Number * -1) < int.MinValue / 10 ||
+                    ((Number * -1) == int.MinValue / 10 && ((sb[Index] - '0') * -1) < -8))
+                    return int.MinValue;
+            }
+
+            Number = Number * 10 + (sb[Index] - '0');
+        }
+
+        return (IsPositive) ? (Number) : (Number * -1);
+    }
+}
+
+
+
+
+
