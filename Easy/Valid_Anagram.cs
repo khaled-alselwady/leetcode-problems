33491ef7eedf3ef0cdc6afa52bc2018697aadb15
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-01-12 05:24:48 +0300

    ADD Valid_Anagram problem #14

---
+++ Easy/Valid_Anagram.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        string s = "arr", t = "rar";
+
+        Console.WriteLine(IsAnagram(s, t));
+        Console.ReadKey();
+    }
+
+    public static bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length)
+            return false;
+
+        Dictionary<char, short> keyValuePairs = new Dictionary<char, short>();
+
+        foreach (char Letter in s)
+        {
+            keyValuePairs.TryGetValue(Letter, out var count);
+            keyValuePairs[Letter] = (short)(count + 1);
+        }
+
+        foreach (char Letter in t)
+        {
+            if (!keyValuePairs.ContainsKey(Letter))
+                return false;
+
+            keyValuePairs[Letter]--;
+
+            if (keyValuePairs[Letter] < 0)
+                return false;
+        }
+
+        return true;
+    }
+
+
+}
+
+
+
+
+
