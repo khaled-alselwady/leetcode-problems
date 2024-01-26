6c7cf335137acab49c578bf4b7aa1676c70356f1
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-01-26 11:07:45 +0300

    ADD Min_Stack problem #20

---
+++ Medium/Min_Stack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        MinStack minStack = new MinStack();
+        minStack.Push(2);
+        minStack.Push(0);
+        minStack.Push(3);
+        minStack.Push(0);
+        Console.WriteLine($"{minStack.GetMin()}"); // return 0
+        minStack.Pop();
+        Console.WriteLine($"{minStack.GetMin()}"); // return 0
+        minStack.Pop();    // return 0
+        Console.WriteLine($"{minStack.GetMin()}"); // return 0
+        minStack.Pop();    // return 0
+        Console.WriteLine($"{minStack.GetMin()}"); // return 2
+
+
+        Console.ReadKey();
+    }
+}
+
+public class MinStack
+{
+    private Stack<int> _TempStack;
+    private Stack<int> _TempMinStack;
+
+    public MinStack()
+    {
+        _TempStack = new Stack<int>();
+        _TempMinStack = new Stack<int>();
+    }
+
+    public void Push(int val)
+    {
+        _TempStack.Push(val);
+
+        int MinNumber = (_TempMinStack.Count > 0) ? Math.Min(val, _TempMinStack.Peek()) : val;
+
+        _TempMinStack.Push(MinNumber);
+    }
+
+    public void Pop()
+    {
+        _TempStack.Pop();
+        _TempMinStack.Pop();
+    }
+
+    public int Top() => _TempStack.Peek();
+
+    public int GetMin() => _TempMinStack.Peek();
+}
