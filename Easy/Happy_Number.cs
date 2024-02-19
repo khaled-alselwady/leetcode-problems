09476ed2a0182c92e0e1709beb5660dc2cd14c32
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-02-19 11:59:57 +0300

    ADD Happy_Number problem #26

---
+++ Easy/Happy_Number.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(IsHappy(100));
+
+        Console.ReadKey();
+    }
+
+    private static int _GetSumSquaresDigitsOfNumber(int Number)
+    {
+        int Sum = 0;
+
+        byte Remainder = 0;
+
+        while (Number > 0)
+        {
+            Remainder = (byte)(Number % 10);
+            Number /= 10;
+
+            Sum += (Remainder * Remainder);
+        }
+
+        return Sum;
+    }
+
+    private static bool _IsNumberExistInHashSet(HashSet<int> Result, int NumberToSearch)
+        => Result.Contains(NumberToSearch);
+
+    public static bool IsHappy(int Number)
+    {
+        if (Number <= 0)
+            return false;
+
+        HashSet<int> SeenNumbers = new HashSet<int>();
+
+        int SumSquaresDigits = Number;
+
+        do
+        {
+            SumSquaresDigits = _GetSumSquaresDigitsOfNumber(SumSquaresDigits);
+
+            if (SumSquaresDigits == 1)
+                return true;
+
+            if (_IsNumberExistInHashSet(SeenNumbers, SumSquaresDigits))
+                return false;
+
+            SeenNumbers.Add(SumSquaresDigits);
+
+        } while (SumSquaresDigits != 1);
+
+        return true;
+    }
+}
+
