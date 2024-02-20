1c9c165c159a087a74de6b924ddf9fe3c06b58cf
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-02-20 19:12:58 +0300

    ADD Power_Of_Two problem #31

---
+++ Easy/Power_Of_Two.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(IsPowerOfTwo_Method1(8));
+
+        Console.ReadKey();
+    }
+
+    public static bool IsPowerOfTwo_Method1(int n)
+    {
+        if (n == 0)
+            return false;
+
+        if (n >= int.MaxValue || n <= int.MinValue)
+            return false;
+
+        return (n & (n - 1)) == 0;
+    }
+
+    public static bool IsPowerOfTwo_Method2(int n)
+    {
+        if (n == 0)
+            return false;
+
+        double result = Math.Round(Math.Log(n, 2));
+
+        return Math.Pow(2, result) == n;       
+    }
+}
+
