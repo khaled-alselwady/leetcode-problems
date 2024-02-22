b0eee93c2d64c9ff90f1425b0adb438f96e257f1
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-02-22 19:53:25 +0300

    ADD Number_Of_1_Bits problem #33

---
+++ Easy/Number_Of_1_Bits.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(13);
+
+        Console.ReadKey();
+    }
+
+    public static int HammingWeight(uint n)
+    {
+        // Convert the integer to its binary representation as a string, then count each 1 using LINQ
+        return Convert.ToString(n, 2).Count(Digit => Digit == '1');
+    }
+}
+
