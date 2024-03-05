e84a56cb88524bcf45b3ecec6771b723bb609b80
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-03-05 17:25:23 +0300

    ADD Find_the_Duplicate_Number problem #40

---
+++ Medium/Find_the_Duplicate_Number.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        int[] nums = { 3, 1, 3, 4, 2 };
+
+        Console.WriteLine(FindDuplicate(nums));
+
+        Console.ReadKey();
+    }
+
+    public static int FindDuplicate(int[] nums)
+    {
+        if (nums == null || nums.Length < 2)
+            return -1;
+
+        int Slow = nums[0];
+        int Fast = nums[0];
+
+        do
+        {
+            Slow = nums[Slow];
+            Fast = nums[nums[Fast]];
+        } while (Slow != Fast);
+
+        Fast = nums[0];
+
+        while (Slow != Fast)
+        {
+            Slow = nums[Slow];
+            Fast = nums[Fast];
+        }
+
+        return Slow;
+    }
+
+}
+
