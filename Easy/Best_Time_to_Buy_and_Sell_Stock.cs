7bac925822a77ecd9209ffc09e70d2afc4021962
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-03-08 22:37:53 +0300

    ADD Best_Time_to_Buy_and_Sell_Stock problem #42

---
+++ Easy/Best_Time_to_Buy_and_Sell_Stock.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        int[] prices = { 2, 4, 1 };
+
+        Console.WriteLine(MaxProfit(prices));
+
+        Console.ReadKey();
+    }
+
+    public static int MaxProfit(int[] prices)
+    {
+        if (prices == null || prices.Length <= 1)
+            return 0;
+
+        int CheaperPrice = prices[0];
+        int MaxProfit = 0;
+
+        for (int i = 1; i < prices.Length; i++)
+        {
+            CheaperPrice = Math.Min(CheaperPrice, prices[i]);
+            MaxProfit = Math.Max(MaxProfit, prices[i] - CheaperPrice);
+        }
+
+        return MaxProfit;
+    }
+
+}
+
