d1b7b324d24239c5db534983cbbc10a3d14cc866
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-04-02 15:15:24 +0300

    ADD Longest_Repeating_Character_Replacement problem #45

---
+++ Medium/Longest_Repeating_Character_Replacement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(CharacterReplacement("AABABBA", 1));
+
+        Console.ReadKey();
+    }
+
+    public static int CharacterReplacement(string s, int k)
+    {
+        if (s == null)
+            return 0;
+
+        int maxLength = 0, mostFrequentLetterCount = 0, start = 0;
+        int[] charCounts = new int[26];
+
+        for (int end = 0; end < s.Length; end++)
+        {
+            charCounts[s[end] - 'A']++;
+
+            mostFrequentLetterCount = Math.Max(mostFrequentLetterCount, charCounts[s[end] - 'A']);
+
+            int lengthWindow = (end - start + 1);
+
+            if ((lengthWindow - mostFrequentLetterCount) > k)
+            {
+                // invalid window
+
+                charCounts[s[start] - 'A']--;
+                start++; // shrink the window
+                lengthWindow--;
+            }
+
+            maxLength = Math.Max(maxLength, lengthWindow);
+        }
+
+        return maxLength;
+    }
+}
+
