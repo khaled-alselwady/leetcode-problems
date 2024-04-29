afb3cdb1ad254db89e57a1fc7551028bfe542996
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-04-29 19:12:18 +0300

    ADD Climbing-Stairs problem #56

---
+++ Easy/climbing-stairs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Solution
+{
+    private static int _GetFibonacciNumber(int n)
+    {
+        int prev = 1;
+        int curr = 1;
+        for (int i = 0; i < n - 1; i++)
+        {
+            int temp = curr;
+            curr += prev;
+            prev = temp;
+        }
+
+        return curr;
+    }
+
+    public static int ClimbStairs(int n)
+    {
+        if (n < 1 || n > 45)
+        {
+            return 0;
+        }
+
+        return _GetFibonacciNumber(n);
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(ClimbStairs(4));
+
+        Console.ReadKey();
+    }
+}
+
