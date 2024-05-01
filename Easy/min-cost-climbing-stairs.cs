e73aec098f7afc6981c773ba6e2c8c824d8a15c1
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-05-01 23:21:56 +0300

    ADD Min-Cost-Climbing-Stairs problem #57

---
+++ Easy/min-cost-climbing-stairs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Solution
+{
+    public static int MinCostClimbingStairs(int[] cost)
+    {
+        if (cost == null || cost.Length < 2)
+        {
+            return 0;
+        }
+
+        for (int i = cost.Length - 3; i >= 0; i--)
+        {
+            cost[i] += Math.Min(cost[i + 1], cost[i + 2]);
+        }
+
+        return Math.Min(cost[0], cost[1]); // Because I can either start from the step with index 0 or 1.
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] cost = { 10, 50 };
+
+        Console.WriteLine(MinCostClimbingStairs(cost));
+
+        Console.ReadKey();
+    }
+}
+
