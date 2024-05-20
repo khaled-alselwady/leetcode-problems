ef3c997520fdad9d303e00dccad66233abcfc9c2
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-05-20 19:23:19 +0300

    ADD Best-Time-To-Buy-And-Sell-Stock-With-Cooldown problem #69

---
+++ Medium/best-time-to-buy-and-sell-stock-with-cooldown.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Solution
+{
+    public static int MaxProfit(int[] prices)
+    {
+        int soldProfit = 0; // Profit when stock is sold today
+        int restProfit = 0; // Profit when we do nothing today
+        int holdProfit = int.MinValue; // Profit when we hold stock today
+
+        for (int i = 0; i < prices.Length; i++)
+        {
+            int previousSoldProfit = soldProfit; // Store previous day's sold profit
+            soldProfit = holdProfit + prices[i]; // Update sold profit by selling stock today
+            holdProfit = Math.Max(holdProfit, restProfit - prices[i]); // Update hold profit
+            restProfit = Math.Max(restProfit, previousSoldProfit); // Update rest profit
+        }
+
+        return Math.Max(soldProfit, restProfit); // Return the maximum profit possible
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] prices = { 1, 2, 3, 0, 2 };
+
+        Console.WriteLine(MaxProfit(prices));
+
+        Console.ReadKey();
+    }
+}
+
