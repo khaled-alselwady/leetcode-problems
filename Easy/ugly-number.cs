8061f88d6afc995464b11dbb968986cae62154a8
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-05-26 19:34:29 +0300

    ADD Ugly-Number problem #75

---
+++ Easy/ugly-number.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public static bool IsUgly(int n)
+    {
+        if (n <= 0)
+        {
+            return false;
+        }
+
+        foreach (byte num in new byte[] { 2, 3, 5 })
+        {
+            while (n % num == 0)
+            {
+                n /= num;
+            }
+        }
+
+        return (n == 1);
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(IsUgly(7));
+    }
+}
+
