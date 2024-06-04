9f3a7644dec351458fbb59e88b159e6b58f39cdd
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-06-04 18:41:33 +0300

    ADD Perfect-Number problem #77

---
+++ Easy/perfect-number.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    private static int _SumAllDivisors(int number)
+    {
+        int sum = 0;
+
+        for (int i = 1; i <= number / 2; i++)
+        {
+            if (number % i == 0)
+            {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+
+    public static bool CheckPerfectNumber(int number)
+        => number == _SumAllDivisors(number);
+
+    static void Main()
+    {
+        Console.WriteLine(CheckPerfectNumber(28));
+    }
+}
+
