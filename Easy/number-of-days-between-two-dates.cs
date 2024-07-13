d0b479d6b4cfad7a97498696b608a08cccb54340
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-07-14 00:35:22 +0300

    feat(problem #80): add number-of-days-between-two-dates

---
+++ Easy/number-of-days-between-two-dates.cs
@@ -0,0 +1,30 @@
+﻿public class Solution
+{
+    private static void SwapDates(ref DateTime date1, ref DateTime date2)
+    {
+        (date1, date2) = (date2, date1);
+    }
+
+    public static int DaysBetweenDates(string date1, string date2)
+    {
+        if (!DateTime.TryParse(date1, out DateTime startDate) ||
+            !DateTime.TryParse(date2, out DateTime endDate))
+        {
+            return 0;
+        }
+
+        if (startDate > endDate)
+        {
+            SwapDates(ref startDate, ref endDate);
+        }
+
+        return (endDate - startDate).Days;
+    }
+
+    private static void Main()
+    {
+        Console.WriteLine(DaysBetweenDates("2019-06-29", "2019-06-30"));
+
+        Console.ReadKey();
+    }
+}
